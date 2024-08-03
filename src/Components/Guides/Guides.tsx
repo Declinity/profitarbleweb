0355a149f68a0307d8811655d98e9e509e9d7fed
declinity <daniel.a.scholes@gmail.com>
2024-08-03 17:19:30 +0200

    symbol fix

--- src/Components/Guides/Guides.tsx
+++ src/Components/Guides/Guides.tsx
@@ -11,19 +11,19 @@ const Guides = () => {
     return (
         <div className="guides-container">
             <h1>Guides</h1>
-            <div className="segment" onClick={() => redirectToGuide('/guides/guide1')}>
+            <div className="segment" onClick={() => redirectToGuide('/guides/introduction')}>
                 <div className="text-section">
                     <h2>Never lose!</h2>
                     <p>Imagine placing £5 on two different bookmakers...</p>
                 </div>
             </div>
-            <div className="segment" onClick={() => redirectToGuide('/guides/guide2')}>
+            <div className="segment" onClick={() => redirectToGuide('/guides/players')}>
                 <div className="text-section">
                     <h2>Never lose!</h2>
                     <p>Imagine placing £5 on two different bookmakers...</p>
                 </div>
             </div>
-            <div className="segment" onClick={() => redirectToGuide('/guides/guide1')}>
+            <div className="segment" onClick={() => redirectToGuide('/guides/cards')}>
                 <div className="text-section">
                     <h2>Never lose!</h2>
                     <p>Imagine placing £5 on two different bookmakers...</p>
